ef23d7923d1d1bd02e49e4dca347d17e067efb0d
huytrnq <huytrq.em@gmail.com>
2024-03-11 22:52:23 +0100

    Intensity Transforms - Linear Stretching

--- ColorImages/IntemsityTransformations/transforms/src/transforms.cpp
+++ ColorImages/IntemsityTransformations/transforms/src/transforms.cpp
@@ -10,7 +10,7 @@ Transforms::~Transforms(){};
  */
 void Transforms::linear_stretching(const cv::Mat input, cv::Mat &output){
     double min, max;
-    // Find the minimum and maximum pixel values
+    //
     cv::minMaxLoc(input, &min, &max);
     // Linear stretching transformation
     output = (input - min) * (255.0 / (max - min));
